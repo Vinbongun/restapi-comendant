e8385f2a24861a75795551b316ce7a26666cb31c
Илюхин Богдан <vinbongun@yandex.ru>
2021-11-10 08:37:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const http = require("http")
 const PORT = process.env.PORT || 5000
+const options = {
+  hostname: "85.30.196.223",
+  port: 8071,
+  path: "/cgi-bin/alarmout_cgi?action=set&user=admin&pwd=Rjvtylfyn2016&Output=0&Status=1%E2%81%A0%E2%81%A0%E2%81%A0%E2%81%A0",
+  method: "GET",
+}
 
 const server = http.createServer(async (req, res) => {
   //set the request route
@@ -10,6 +16,18 @@ const server = http.createServer(async (req, res) => {
     res.write("Сервер стартанул")
     //end the response
     res.end()
+    
+    const req = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`)
+    res.on("data", (d) => {
+      process.stdout.write(d)
+      })
+    })
+    req.on("error", (error) => {
+      console.error(error)
+    })
+    req.end()
+    
   }
 
   // If no route present
